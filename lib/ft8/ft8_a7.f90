3d06f9a9dca37419368975d3fa17cf0ca7444a4f
boybook <fangyizhou1997@qq.com>
2026-05-04 02:08:10 +0800

    upgrade: port WSJT-X 3.0.0 FT8 decoder enhancements
    ft8_decode.f90: MAXCAND=1000, MAX_EARLY=200, ltry_a8, hisgrid params
    ft8b.f90: 5-channel soft bits (bmete), 13 AP passes, dynamic syncmin
    ft8_a7.f90: updated to 3.0.0
    Add ft8_a8d.f90 (list decoding with DxCall/DxGrid)
    Add ft8q3.f90 (q3 quality assessment)
    sync8.f90, ft8_downsample.f90, gen_ft8wave.f90: updated to 3.0.0
    decoder.f90: pass ltry_a8 and hisgrid to FT8 decode
    Infrastructure: thread_local msg queue, setDxCall/setDxGrid, parameterized nfa/nfb/ntol

--- lib/ft8/ft8_a7.f90
+++ lib/ft8/ft8_a7.f90
@@ -1,6 +1,6 @@
 module ft8_a7
 
-  parameter(MAXDEC=100)
+  parameter(MAXDEC=200)
 
 ! For the following three arrays
 !    First index   i=decode number in this sequence
@@ -17,7 +17,7 @@ module ft8_a7
 
 contains
 
-subroutine ft8_a7_save(nutc,dt,f,msg)
+subroutine ft8_a7_save(jseq,dt,f,msg)
 
   use packjt77
   character*37 msg,msg1
@@ -37,13 +37,12 @@ subroutine ft8_a7_save(nutc,dt,f,msg)
   call split77(msg,nwords,nw,w)          !Parse msg into words
   if(nwords.lt.1) go to 999
   if(w(1)(1:3).eq.'CQ_') go to 999
-  j=mod(nutc/5,2)                        !j is 0 or 1 for odd/even sequence
-  jseq=j
+  j=jseq
 
 ! Add this decode to current table for this sequence
   ndec(j,1)=ndec(j,1)+1                  !Number of decodes in this sequence
   i=ndec(j,1)                            !i is index of a new table entry
-  if(i.ge.MAXDEC-1) return               !Prevent table overflow
+  if(i.gt.MAXDEC) return                 !Prevent table overflow (indexes start at 1)
 
   dt0(i,j,1)=dt                          !Save dt in table
   f0(i,j,1)=f                            !Save f in table
@@ -362,9 +361,9 @@ subroutine ft8_a7d(dd0,newdat,call_1,call_2,grid4,xdt,f1,xbase,nharderrors,dmin,
   dmm(iloc(1))=1.e30
   iloc=minloc(dmm)
   dmin2=dmm(iloc(1))
-  xsnr=-24.
+  xsnr=-25.
   arg=pbest/xbase/3.0e6-1.0
-  if(arg.gt.0.0) xsnr=max(-24.0,db(arg)-27.0)
+  if(arg.gt.0.0) xsnr=max(-25.0,db(arg)-27.0)
 !  write(41,3041) nharderrors,dmin,dmin2,dmin2/dmin,xsnr,trim(msgbest)
 !3041 format(i3,2f7.1,f7.2,f7.1,1x,a)
   if(dmin.gt.100.0 .or. dmin2/dmin.lt.1.3) nharderrors=-1
@@ -374,5 +373,49 @@ subroutine ft8_a7d(dd0,newdat,call_1,call_2,grid4,xdt,f1,xbase,nharderrors,dmin,
 
   return
 end subroutine ft8_a7d
+
+subroutine getmsg(i,mycall,hiscall,hisgrid,msg)
+
+  character*12 mycall,hiscall
+  character*6 hisgrid
+  character*37 msg
+  logical my_std,his_std
+
+  call stdcall(mycall,my_std)
+  call stdcall(hiscall,his_std)
+
+  isnr=0
+  msg=trim(mycall)//' '//trim(hiscall)
+  if(.not.my_std) then
+     if(i.eq.1 .or. i.ge.6)  msg='<'//trim(mycall)//'> '//trim(hiscall)
+     if(i.ge.2 .and. i.le.4) msg=trim(mycall)//' <'//trim(hiscall)//'>'
+  else if(.not.his_std) then
+     if(i.le.4 .or. i.eq.6) msg='<'//trim(mycall)//'> '//trim(hiscall)
+     if(i.ge.7) msg=trim(mycall)//' <'//trim(hiscall)//'>'
+  endif
+  j0=len(trim(msg))+2
+  if(i.eq.2) msg(j0:j0+2)='RRR'
+  if(i.eq.3) msg(j0:j0+3)='RR73'
+  if(i.eq.4) msg(j0:j0+1)='73'
+  if(i.eq.5) then
+     if(his_std) msg='CQ '//trim(hiscall)//' '//hisgrid(1:4)
+     if(.not.his_std) msg='CQ '//trim(hiscall)
+  endif
+  if(i.eq.6 .and. his_std) msg(j0:j0+3)=hisgrid(1:4)
+  if(i.ge.7 .and. i.le.206) then
+     isnr = -50 + (i-7)/2
+     if(iand(i,1).eq.1) then
+        write(msg(j0:j0+2),'(i3.2)') isnr
+        if(msg(j0:j0).eq.' ') msg(j0:j0)='+'
+     else
+        write(msg(j0:j0+3),'("R",i3.2)') isnr
+        if(msg(j0+1:j0+1).eq.' ') msg(j0+1:j0+1)='+'
+     endif
+  endif
+
+  if(abs(isnr).gt.30) msg=''
+
+  return
+end subroutine getmsg
 
 end module ft8_a7
--- wsjtx_lib.h
+++ wsjtx_lib.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <complex>
+#include "DataBuffer.h"
 
 typedef std::vector<float> WsjTxVector;
 typedef std::vector<short int> IntWsjTxVector;
@@ -26,49 +27,28 @@ class decoder_results
 class decoder_options
 {
   public:
-	int freq;		  // Dial frequency
-	char rcall[13];   // Callsign of the RX station
-	char rloc[7];	 // Locator of the RX station
-	int quickmode;	// Decoder option & tweak
-	int usehashtable; //  ''
-	int npasses;	  //  ''
-	int subtraction;  //  ''
-
+	int freq;
+	char rcall[13];
+	char rloc[7];
+	int quickmode;
+	int usehashtable;
+	int npasses;
+	int subtraction;
 	decoder_options();
 };
 
-enum wsjtxMode
-{
-	FT8,
-	FT4,
-	JT4,
-	JT65,
-	JT9,
-	FST4,
-	Q65,
-	FST4W,
-	JT65JT9, // Dual
-	WSPR
-};
+enum wsjtxMode { FT8, FT4, JT4, JT65, JT9, FST4, Q65, FST4W, JT65JT9, WSPR };
 
 class WsjtxMessage
 {
   public:
 	WsjtxMessage(int chh, int cmm, int css, int csnr, float cdt, int cfreq, std::string cmsg)
 		: hh{chh}, min{cmm}, sec{css}, snr{csnr}, dt{cdt}, freq{cfreq}, msg{cmsg}, sync{0.0} {};
-
-	WsjtxMessage()
-		: hh{0}, min{0}, sec{0}, snr{0}, freq{0}, dt{0}, msg{""} {};
-
+	WsjtxMessage() : hh{0}, min{0}, sec{0}, snr{0}, freq{0}, dt{0}, msg{""} {};
 	WsjtxMessage(int chh, int cmm, int css, int csnr, float csync, float cdt, int cfreq, std::string cmsg)
 		: hh{chh}, min{cmm}, sec{css}, snr{csnr}, sync{csync}, dt{cdt}, freq{cfreq}, msg{cmsg} {};
-
-	int hh;
-	int min;
-	int sec;
-	int snr;
-	float sync;
-	float dt;
+	int hh, min, sec, snr;
+	float sync, dt;
 	int freq;
 	std::string msg;
 };
@@ -77,11 +57,15 @@ class wsjtx_lib
 {
   public:
 	wsjtx_lib();
-
 	void decode(wsjtxMode mode, WsjTxVector &audiosamples, int freq, int thread = 1);
 	void decode(wsjtxMode mode, IntWsjTxVector &audiosamples, int freq, int thread = 1);
 	std::vector<float> encode(wsjtxMode mode, int frequency, std::string message, std::string &messagesend);
 	bool pullMessage(WsjtxMessage &msg);
 	std::vector<decoder_results> wspr_decode(WsjtxIQSampleVector &iqsignal, decoder_options options);
+	void setDxCall(const std::string& call);
+	void setDxGrid(const std::string& grid);
+  private:
+	std::string dx_call_;
+	std::string dx_grid_;
+	DataQueue<WsjtxMessage> messageQueue_;
 };
-
